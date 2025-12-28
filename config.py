ab8e00dd471a9682b95102f52c26c3ccc291d5d3
Recep Inan <recinan.ceng@gmail.com>
2025-12-28 13:25:22 +0300

    now backend using the database that dbpath determined by electron.

--- config.py
+++ config.py
@@ -7,5 +7,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
-    INSTANCE_PATH = os.path.join(BASE_DIR, "instance")
-    VAULT_DB_PATH = os.path.join(INSTANCE_PATH, "passman.db")
+    # INSTANCE_PATH = os.path.join(BASE_DIR, "instance")
+    VAULT_DB_PATH = os.getenv(
+        "VAULT_DB_PATH",
+        os.path.join(os.path.dirname(__file__), "instance","passman.db")
+    )
